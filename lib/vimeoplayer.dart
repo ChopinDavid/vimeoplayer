2327fb23b1bc1a79fb0c3eaab642dc50de93da9f
DavidC <David.Chopin@wwt.com>
2020-08-21 12:28:51 -0500

    Made the widget more configurable

--- lib/vimeoplayer.dart
+++ lib/vimeoplayer.dart
@@ -1,16 +1,22 @@
 library vimeoplayer;
 
-import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
-import 'package:flutter/services.dart';
-import 'src/quality_links.dart';
 import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+
+import 'src/quality_links.dart';
 
 class VimeoPlayer extends StatefulWidget {
   final String id;
+  final Color iconColor;
+  final bool fullscreenable;
 
   VimeoPlayer({
     @required this.id,
+    this.iconColor,
+    this.fullscreenable = true,
     Key key,
   }) : super(key: key);
 
@@ -74,10 +80,9 @@ class _VimeoPlayerState extends State<VimeoPlayer> {
                   return LayoutBuilder(
                     builder: (context, constraints) {
                       double delta = constraints.maxWidth -
-                          constraints.maxHeight *
-                              _controller.value.aspectRatio;
+                          constraints.maxHeight * _controller.value.aspectRatio;
                       if (MediaQuery.of(context).orientation ==
-                          Orientation.portrait ||
+                              Orientation.portrait ||
                           delta < 0) {
                         videoHeight = constraints.maxWidth /
                             _controller.value.aspectRatio;
@@ -85,9 +90,9 @@ class _VimeoPlayerState extends State<VimeoPlayer> {
                         videoMargin = 0;
                       } else {
                         videoHeight = constraints.maxHeight - 36;
-                        videoWidth = videoHeight * _controller.value.aspectRatio;
-                        videoMargin =
-                            (constraints.maxWidth - videoWidth) / 2;
+                        videoWidth =
+                            videoHeight * _controller.value.aspectRatio;
+                        videoMargin = (constraints.maxWidth - videoWidth) / 2;
                       }
 
                       if (_seek && _controller.value.duration.inSeconds > 2) {
@@ -188,8 +193,20 @@ class _VimeoPlayerState extends State<VimeoPlayer> {
                         top: videoHeight / 2 - 30,
                         bottom: videoHeight / 2 - 30),
                     icon: _controller.value.isPlaying
-                        ? Icon(Icons.pause, size: 60.0)
-                        : Icon(Icons.play_arrow, size: 60.0),
+                        ? Icon(
+                            Icons.pause,
+                            size: 60.0,
+                            color: widget.iconColor != null
+                                ? widget.iconColor
+                                : Theme.of(context).iconTheme.color,
+                          )
+                        : Icon(
+                            Icons.play_arrow,
+                            size: 60.0,
+                            color: widget.iconColor != null
+                                ? widget.iconColor
+                                : Theme.of(context).iconTheme.color,
+                          ),
                     onPressed: () {
                       setState(() {
                         _controller.value.isPlaying
@@ -198,31 +215,45 @@ class _VimeoPlayerState extends State<VimeoPlayer> {
                       });
                     }),
               ),
-              Container(
-                margin: EdgeInsets.only(
-                    top: videoHeight - 70, left: videoWidth + videoMargin - 50),
-                child: IconButton(
-                    alignment: AlignmentDirectional.center,
-                    icon: Icon(Icons.fullscreen, size: 30.0),
-                    onPressed: () {
-                      setState(() {
-                        MediaQuery.of(context).orientation ==
-                                Orientation.landscape
-                            ? SystemChrome.setPreferredOrientations([
-                                DeviceOrientation.portraitDown,
-                                DeviceOrientation.portraitUp
-                              ])
-                            : SystemChrome.setPreferredOrientations([
-                                DeviceOrientation.landscapeLeft,
-                                DeviceOrientation.landscapeRight
-                              ]);
-                      });
-                    }),
-              ),
+              if (widget.fullscreenable)
+                Container(
+                  margin: EdgeInsets.only(
+                      top: videoHeight - 70,
+                      left: videoWidth + videoMargin - 50),
+                  child: IconButton(
+                      alignment: AlignmentDirectional.center,
+                      icon: Icon(
+                        Icons.fullscreen,
+                        size: 30.0,
+                        color: widget.iconColor != null
+                            ? widget.iconColor
+                            : Theme.of(context).iconTheme.color,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          MediaQuery.of(context).orientation ==
+                                  Orientation.landscape
+                              ? SystemChrome.setPreferredOrientations([
+                                  DeviceOrientation.portraitDown,
+                                  DeviceOrientation.portraitUp
+                                ])
+                              : SystemChrome.setPreferredOrientations([
+                                  DeviceOrientation.landscapeLeft,
+                                  DeviceOrientation.landscapeRight
+                                ]);
+                        });
+                      }),
+                ),
               Container(
                 margin: EdgeInsets.only(left: videoWidth + videoMargin - 48),
                 child: IconButton(
-                    icon: Icon(Icons.settings, size: 26.0),
+                    icon: Icon(
+                      Icons.settings,
+                      size: 26.0,
+                      color: widget.iconColor != null
+                          ? widget.iconColor
+                          : Theme.of(context).iconTheme.color,
+                    ),
                     onPressed: () {
                       _position = _controller.value.position;
                       _seek = true;
@@ -293,7 +324,8 @@ class _VimeoPlayerState extends State<VimeoPlayer> {
                 child: Text(value.duration.inMinutes.toString() +
                     ':' +
                     (value.duration.inSeconds - value.duration.inMinutes * 60)
-                        .toString()),
+                        .toString()
+                        .padLeft(2, '0')),
               ),
             ],
           );
